8a6b9baa753d02cfebb99487b3722d448be606a4
Zelda Fan <zeldafan42@gmail.com>
2015-05-23 15:37:36 +0200

    Inital commit

---
+++ Roboter-Labyrinth/Maze.cpp
@@ -0,0 +1,18 @@
+/*
+ * Maze.cpp
+ *
+ *  Created on: 23.05.2015
+ *      Author: Christine
+ */
+
+#include "Maze.hpp"
+
+Maze::Maze() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Maze::~Maze() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Roboter-Labyrinth/Maze.hpp
@@ -0,0 +1,17 @@
+/*
+ * Maze.hpp
+ *
+ *  Created on: 23.05.2015
+ *      Author: Christine
+ */
+
+#ifndef MAZE_HPP_
+#define MAZE_HPP_
+
+class Maze {
+public:
+	Maze();
+	virtual ~Maze();
+};
+
+#endif /* MAZE_HPP_ */
